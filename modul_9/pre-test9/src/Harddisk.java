d0e131ef7abf2a92f48015c45dbcf5206cdebf4f
zulka1 <ubaidputra2@gmail.com>
2026-05-19 14:27:24 +0700

    pretest

---
+++ modul_9/pre-test9/src/Harddisk.java
@@ -0,0 +1,9 @@
+public class Harddisk {
+    int speed;
+    int capacity;
+
+    public Harddisk(int speed, int capacity) {
+        this.speed = speed;
+        this.capacity = capacity;
+    }
+}
---
+++ modul_9/pre-test9/src/KomputerServer.java
@@ -0,0 +1,7 @@
+public class KomputerServer {
+    private Harddisk h;
+
+    public KomputerServer() {
+        this.h = new Harddisk(9500, 1000);
+    }
+}
